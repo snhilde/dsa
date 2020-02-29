905df7ec9cfade02b9c7288af9c9df6cd7251376
Hilde N <snhilde@protonmail.com>
2020-02-29 11:18:01 -0500

    testString() -> checkString(), testLength() -> checkLength()

--- data_structures/hlist/hlist_test.go
+++ data_structures/hlist/hlist_test.go
@@ -32,24 +32,24 @@ func TestAppend(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "5")
-	testLength(t, list, 1)
+	checkString(t, list, "5")
+	checkLength(t, list, 1)
 
 	// Add another item. Test that item was added successfully and that list is displayed correctly.
 	err = list.Append("bananas")
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "5, bananas")
-	testLength(t, list, 2)
+	checkString(t, list, "5, bananas")
+	checkLength(t, list, 2)
 
 	// Add another item. Test that item was added successfully and that list is displayed correctly.
 	err = list.Append([]float64{3.14, 1.23})
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "5, bananas, [3.14 1.23]")
-	testLength(t, list, 3)
+	checkString(t, list, "5, bananas, [3.14 1.23]")
+	checkLength(t, list, 3)
 }
 
 func TestInsert(t *testing.T) {
@@ -64,8 +64,8 @@ func TestInsert(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "5")
-	testLength(t, list, 1)
+	checkString(t, list, "5")
+	checkLength(t, list, 1)
 
 	// Insert another item at the beginning. Test that item was added successfully and that list is displayed correctly
 	// and length is correct.
@@ -73,8 +73,8 @@ func TestInsert(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "bananas, 5")
-	testLength(t, list, 2)
+	checkString(t, list, "bananas, 5")
+	checkLength(t, list, 2)
 
 	// Add an item in the middle. Test that item was added successfully and that list is displayed correctly and length
 	// is correct.
@@ -82,24 +82,24 @@ func TestInsert(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "bananas, [3.14 1.23], 5")
-	testLength(t, list, 3)
+	checkString(t, list, "bananas, [3.14 1.23], 5")
+	checkLength(t, list, 3)
 
 	// Now, test incorrect usage.
 	err = list.Insert("no way", 10)
 	if err == nil {
 		t.Error("Unexpectedly passed out-of-bounds test")
 	}
-	testString(t, list, "bananas, [3.14 1.23], 5")
-	testLength(t, list, 3)
+	checkString(t, list, "bananas, [3.14 1.23], 5")
+	checkLength(t, list, 3)
 
 	var bad_list *Hlist
 	err = bad_list.Insert("shouldn't work", 0)
 	if err == nil {
 		t.Error("Unexpectedly passed invalid list test")
 	}
-	testString(t, bad_list, "<nil>")
-	testLength(t, bad_list, -1)
+	checkString(t, bad_list, "<nil>")
+	checkLength(t, bad_list, -1)
 }
 
 func TestPop(t *testing.T) {
@@ -111,8 +111,8 @@ func TestPop(t *testing.T) {
 	list.Append(5)
 
 	// Make sure the list was set up correctly.
-	testString(t, list, "1, 2, 3, 4, 5")
-	testLength(t, list, 5)
+	checkString(t, list, "1, 2, 3, 4, 5")
+	checkLength(t, list, 5)
 
 	// Pop the 3rd item.
 	value := list.Pop(2)
@@ -121,8 +121,8 @@ func TestPop(t *testing.T) {
 		t.Log("Expected: 3")
 		t.Log("Received:", value)
 	}
-	testString(t, list, "1, 2, 4, 5")
-	testLength(t, list, 4)
+	checkString(t, list, "1, 2, 4, 5")
+	checkLength(t, list, 4)
 
 	// Pop the new 3rd item.
 	value = list.Pop(2)
@@ -131,8 +131,8 @@ func TestPop(t *testing.T) {
 		t.Log("Expected: 4")
 		t.Log("Received:", value)
 	}
-	testString(t, list, "1, 2, 5")
-	testLength(t, list, 3)
+	checkString(t, list, "1, 2, 5")
+	checkLength(t, list, 3)
 
 	// Try to pop a non-existant item.
 	value = list.Pop(10)
@@ -141,8 +141,8 @@ func TestPop(t *testing.T) {
 		t.Log("Expected: nil")
 		t.Log("Received:", value)
 	}
-	testString(t, list, "1, 2, 5")
-	testLength(t, list, 3)
+	checkString(t, list, "1, 2, 5")
+	checkLength(t, list, 3)
 
 	// Try the pop operation on an invalid list.
 	var bad_list *Hlist
@@ -163,48 +163,48 @@ func TestPopMatch(t *testing.T) {
 	list.Append(3.14)
 
 	// Make sure the list was set up correctly.
-	testString(t, list, "1, apples, 3, 4, 3.14")
-	testLength(t, list, 5)
+	checkString(t, list, "1, apples, 3, 4, 3.14")
+	checkLength(t, list, 5)
 
 	// Pop the "3".
 	ret := list.PopMatch(3)
 	if !ret {
 		t.Error("Error popping \"3\": no match")
 	}
-	testString(t, list, "1, apples, 4, 3.14")
-	testLength(t, list, 4)
+	checkString(t, list, "1, apples, 4, 3.14")
+	checkLength(t, list, 4)
 
 	// Pop the "1".
 	ret = list.PopMatch(1)
 	if !ret {
 		t.Error("Error popping \"1\": no match")
 	}
-	testString(t, list, "apples, 4, 3.14")
-	testLength(t, list, 3)
+	checkString(t, list, "apples, 4, 3.14")
+	checkLength(t, list, 3)
 
 	// Pop "apples".
 	ret = list.PopMatch("apples")
 	if !ret {
 		t.Error("Error popping \"apples\": no match")
 	}
-	testString(t, list, "4, 3.14")
-	testLength(t, list, 2)
+	checkString(t, list, "4, 3.14")
+	checkLength(t, list, 2)
 
 	// Pop the "3.14".
 	ret = list.PopMatch(3.14)
 	if !ret {
 		t.Error("Error popping \"3.14\": no match")
 	}
-	testString(t, list, "4")
-	testLength(t, list, 1)
+	checkString(t, list, "4")
+	checkLength(t, list, 1)
 
 	// Try to pop a non-existant item.
 	ret = list.PopMatch(10)
 	if ret {
 		t.Error("Unexpectedly passed no-match test")
 	}
-	testString(t, list, "4")
-	testLength(t, list, 1)
+	checkString(t, list, "4")
+	checkLength(t, list, 1)
 
 	// Try the pop operation on an invalid list.
 	var bad_list *Hlist
@@ -223,8 +223,8 @@ func TestIndex(t *testing.T) {
 	list.Append(4)
 
 	// Make sure the list was set up correctly.
-	testString(t, list, "apples, 1, 3, 3.14, 4")
-	testLength(t, list, 5)
+	checkString(t, list, "apples, 1, 3, 3.14, 4")
+	checkLength(t, list, 5)
 
 	// Check index of 1.
 	index := list.Index(1)
@@ -273,8 +273,8 @@ func TestExists(t *testing.T) {
 	list.Append(4)
 
 	// Make sure the list was set up correctly.
-	testString(t, list, "1, 3, apples, 3.14, 4")
-	testLength(t, list, 5)
+	checkString(t, list, "1, 3, apples, 3.14, 4")
+	checkLength(t, list, 5)
 
 	// Check that 3 exists.
 	ret := list.Exists(3)
@@ -314,63 +314,63 @@ func TestMerge(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		list.Append(i)
 	}
-	testString(t, list, "0, 1, 2, 3, 4")
-	testLength(t, list, 5)
+	checkString(t, list, "0, 1, 2, 3, 4")
+	checkLength(t, list, 5)
 
 	addition := New()
 	for i := 5; i < 10; i++ {
 		addition.Append(i)
 	}
-	testString(t, addition, "5, 6, 7, 8, 9")
-	testLength(t, addition, 5)
+	checkString(t, addition, "5, 6, 7, 8, 9")
+	checkLength(t, addition, 5)
 
 	err := list.Merge(addition)
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "0, 1, 2, 3, 4, 5, 6, 7, 8, 9")
-	testString(t, addition, "<empty>")
-	testLength(t, addition, 0)
+	checkString(t, list, "0, 1, 2, 3, 4, 5, 6, 7, 8, 9")
+	checkString(t, addition, "<empty>")
+	checkLength(t, addition, 0)
 
 	// Test merging a good list and a bad list.
 	list = New()
 	for i := 0; i < 5; i++ {
 		list.Append(i)
 	}
-	testString(t, list, "0, 1, 2, 3, 4")
-	testLength(t, list, 5)
+	checkString(t, list, "0, 1, 2, 3, 4")
+	checkLength(t, list, 5)
 
 	var bad_list *Hlist
-	testString(t, bad_list, "<nil>")
-	testLength(t, bad_list, -1)
+	checkString(t, bad_list, "<nil>")
+	checkLength(t, bad_list, -1)
 
 	err = list.Merge(bad_list)
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "0, 1, 2, 3, 4")
-	testLength(t, list, 5)
+	checkString(t, list, "0, 1, 2, 3, 4")
+	checkLength(t, list, 5)
 
 	// Test merging a bad list with a good list.
 	// var bad_list *Hlist
-	testString(t, bad_list, "<nil>")
-	testLength(t, bad_list, -1)
+	checkString(t, bad_list, "<nil>")
+	checkLength(t, bad_list, -1)
 
 	list = New()
 	for i := 0; i < 5; i++ {
 		list.Append(i)
 	}
-	testString(t, list, "0, 1, 2, 3, 4")
-	testLength(t, list, 5)
+	checkString(t, list, "0, 1, 2, 3, 4")
+	checkLength(t, list, 5)
 
 	err = bad_list.Merge(list)
 	if err == nil {
 		t.Error("Unexpectedly passed bad merge")
 	}
-	testString(t, bad_list, "<nil>")
-	testLength(t, bad_list, -1)
-	testString(t, list, "0, 1, 2, 3, 4")
-	testLength(t, list, 5)
+	checkString(t, bad_list, "<nil>")
+	checkLength(t, bad_list, -1)
+	checkString(t, list, "0, 1, 2, 3, 4")
+	checkLength(t, list, 5)
 }
 
 func TestSortInt(t *testing.T) {
@@ -379,87 +379,87 @@ func TestSortInt(t *testing.T) {
 	for i := 1; i <= 8; i++ {
 		list.Insert(i, 0)
 	}
-	testString(t, list, "8, 7, 6, 5, 4, 3, 2, 1")
-	testLength(t, list, 8)
+	checkString(t, list, "8, 7, 6, 5, 4, 3, 2, 1")
+	checkLength(t, list, 8)
 	err := list.SortInt()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "1, 2, 3, 4, 5, 6, 7, 8")
-	testLength(t, list, 8)
+	checkString(t, list, "1, 2, 3, 4, 5, 6, 7, 8")
+	checkLength(t, list, 8)
 
 	// Test a block with less than one full stack.
 	list = New()
 	for i := 1; i <= 9; i++ {
 		list.Insert(i, 0)
 	}
-	testString(t, list, "9, 8, 7, 6, 5, 4, 3, 2, 1")
-	testLength(t, list, 9)
+	checkString(t, list, "9, 8, 7, 6, 5, 4, 3, 2, 1")
+	checkLength(t, list, 9)
 	err = list.SortInt()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "1, 2, 3, 4, 5, 6, 7, 8, 9")
-	testLength(t, list, 9)
+	checkString(t, list, "1, 2, 3, 4, 5, 6, 7, 8, 9")
+	checkLength(t, list, 9)
 
 	// Test a block with more than one full stack but less than a full block
 	list = New()
 	for i := 1; i <= 11; i++ {
 		list.Insert(i, 0)
 	}
-	testString(t, list, "11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1")
-	testLength(t, list, 11)
+	checkString(t, list, "11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1")
+	checkLength(t, list, 11)
 	err = list.SortInt()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11")
-	testLength(t, list, 11)
+	checkString(t, list, "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11")
+	checkLength(t, list, 11)
 
 	// Test small list sizes.
 	list = New()
 	list.Append(1)
 	list.Append(0)
-	testString(t, list, "1, 0")
-	testLength(t, list, 2)
+	checkString(t, list, "1, 0")
+	checkLength(t, list, 2)
 	err = list.SortInt()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "0, 1")
-	testLength(t, list, 2)
+	checkString(t, list, "0, 1")
+	checkLength(t, list, 2)
 
 	list = New()
 	list.Append(1)
-	testString(t, list, "1")
-	testLength(t, list, 1)
+	checkString(t, list, "1")
+	checkLength(t, list, 1)
 	err = list.SortInt()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "1")
-	testLength(t, list, 1)
+	checkString(t, list, "1")
+	checkLength(t, list, 1)
 
 	// Test invalid lists.
 	list = New()
-	testString(t, list, "<empty>")
-	testLength(t, list, 0)
+	checkString(t, list, "<empty>")
+	checkLength(t, list, 0)
 	err = list.SortInt()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "<empty>")
-	testLength(t, list, 0)
+	checkString(t, list, "<empty>")
+	checkLength(t, list, 0)
 
 	var bad_list *Hlist
-	testString(t, bad_list, "<nil>")
-	testLength(t, bad_list, -1)
+	checkString(t, bad_list, "<nil>")
+	checkLength(t, bad_list, -1)
 	err = bad_list.SortInt()
 	if err == nil {
 		t.Error("Unexpectedly passed bad list sort")
 	}
-	testString(t, bad_list, "<nil>")
-	testLength(t, bad_list, -1)
+	checkString(t, bad_list, "<nil>")
+	checkLength(t, bad_list, -1)
 }
 
 func TestSortStr(t *testing.T) {
@@ -468,62 +468,62 @@ func TestSortStr(t *testing.T) {
 	list.Append("ccc")
 	list.Append("bbb")
 	list.Append("aaa")
-	testString(t, list, "ccc, bbb, aaa")
-	testLength(t, list, 3)
+	checkString(t, list, "ccc, bbb, aaa")
+	checkLength(t, list, 3)
 	err := list.SortStr()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "aaa, bbb, ccc")
-	testLength(t, list, 3)
+	checkString(t, list, "aaa, bbb, ccc")
+	checkLength(t, list, 3)
 
 	// Test variable lengths and no same characters.
 	list = New()
 	list.Append("ccccc")
 	list.Append("bbb")
 	list.Append("aaaaaaaaaaa")
-	testString(t, list, "ccccc, bbb, aaaaaaaaaaa")
-	testLength(t, list, 3)
+	checkString(t, list, "ccccc, bbb, aaaaaaaaaaa")
+	checkLength(t, list, 3)
 	err = list.SortStr()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "aaaaaaaaaaa, bbb, ccccc")
-	testLength(t, list, 3)
+	checkString(t, list, "aaaaaaaaaaa, bbb, ccccc")
+	checkLength(t, list, 3)
 
 	// Test uniform length and similar characters.
 	list = New()
 	list.Append("cabc")
 	list.Append("caab")
 	list.Append("abab")
-	testString(t, list, "cabc, caab, abab")
-	testLength(t, list, 3)
+	checkString(t, list, "cabc, caab, abab")
+	checkLength(t, list, 3)
 	err = list.SortStr()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "abab, caab, cabc")
-	testLength(t, list, 3)
+	checkString(t, list, "abab, caab, cabc")
+	checkLength(t, list, 3)
 
 	// Test variable length and similar characters.
 	list = New()
 	list.Append("cababcabbcbababca")
 	list.Append("cabacbcabacb")
 	list.Append("cabababacba")
-	testString(t, list, "cababcabbcbababca, cabacbcabacb, cabababacba")
-	testLength(t, list, 3)
+	checkString(t, list, "cababcabbcbababca, cabacbcabacb, cabababacba")
+	checkLength(t, list, 3)
 	err = list.SortStr()
 	if err != nil {
 		t.Error(err)
 	}
-	testString(t, list, "cabababacba, cababcabbcbababca, cabacbcabacb")
-	testLength(t, list, 3)
+	checkString(t, list, "cabababacba, cababcabbcbababca, cabacbcabacb")
+	checkLength(t, list, 3)
 }
 
 
 
 // HELPERS
-func testString(t *testing.T, list *Hlist, want string) {
+func checkString(t *testing.T, list *Hlist, want string) {
 	if fmt.Sprintf("%v", list) != want {
 		t.Error("List contents are incorrect")
 		t.Log("Expected:", want)
@@ -531,7 +531,7 @@ func testString(t *testing.T, list *Hlist, want string) {
 	}
 }
 
-func testLength(t *testing.T, list *Hlist, want int) {
+func checkLength(t *testing.T, list *Hlist, want int) {
 	if list.Length() != want {
 		t.Error("Incorrect length")
 		t.Log("Expected:", want)
