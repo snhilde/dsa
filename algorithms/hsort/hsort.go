4d1d2c891f79ecdb52865a1bc292e98dfe1d84f7
Hilde N <snhilde@protonmail.com>
2020-03-17 21:10:21 -0400

    add InsertionInt() and SelectionInt() back in

--- algorithms/hsort/hsort.go
+++ algorithms/hsort/hsort.go
@@ -36,6 +36,35 @@ func Insertion(list interface{}) error {
 	return nil
 }
 
+// Sort the list of ints using an insertion algorithm.
+func InsertionInt(list []int) error {
+	// We're going to follow this sequence for each item in the list:
+	// 1. Get the value at the current index.
+	// 2. For all previous items--starting from the current position and going down to the beginning--
+	//    if the item at the index has a greater value, then shift it one to the right.
+	// 3. Insert the value at the now-open index.
+	if len(list) < 1 {
+		return errors.New("Invalid list size")
+	}
+
+	for i, v := range list {
+		for i > 0 {
+			// Scan down the section of the list that is now sorted.
+			previous := list[i-1]
+			if previous > v {
+				// Shift one to the right.
+				list[i] = previous
+				i--
+			} else {
+				break
+			}
+		}
+		list[i] = v
+	}
+
+	return nil
+}
+
 // Sort the list using a selection algorithm. The list must be a slice or array of a uniform data type.
 func Selection(list interface{}) error {
 	// We're going to follow this sequence for each item in the list:
@@ -59,6 +88,32 @@ func Selection(list interface{}) error {
 		}
 		// Swap the selected value with the current value.
 		swap(i, pos)
+	}
+
+	return nil
+}
+
+// Sort the list of ints using a selection algorithm.
+func SelectionInt(list []int) error {
+	// We're going to follow this sequence for each item in the list:
+	// 1. Scan the entire list from the current position forward for the lowest value.
+	// 2. Swap the current value and the lowest value.
+	length := len(list)
+	if length < 1 {
+		return errors.New("Invalid list size")
+	}
+
+	for i := range list {
+		pos := i
+		for j := i+1; j < length; j++ {
+			// Check each value to see if it's lower than our current lowest.
+			if list[j] < list[pos] {
+				// We found a value lower than we currently have. Select it.
+				pos = j
+			}
+		}
+		// Swap the selected value with the current value.
+		list[i], list[pos] = list[pos], list[i]
 	}
 
 	return nil
