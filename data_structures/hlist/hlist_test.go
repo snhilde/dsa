ec6ab375337ad15b3f8d8d1f644cd5d76f9eea03
Hilde N <snhilde@protonmail.com>
2020-03-23 07:17:36 -0400

    begin rewriting tests, starting with bad pointer and bad argument tests

--- data_structures/hlist/hlist_test.go
+++ data_structures/hlist/hlist_test.go
@@ -6,629 +6,153 @@ import (
 
 
 // TESTS
-func TestNew(t *testing.T) {
-	// Test the correct way to create a new linked list.
-	good_list := New()
-	if good_list == nil {
-		t.Error("Failed to create new list")
+func TestBadPtr(t *testing.T) {
+	// Test that using a non-initialized list is handled correctly.
+	var l *List
+
+    // Test String().
+	if s := l.String(); s != "<nil>" {
+		t.Error("unexpectedly passed String() test with bad pointer")
+		t.Log("\tExpected: <nil>")
+		t.Log("\tReceived:", s)
 	}
 
-	// Test the incorrect way to create a new linked list.
-	var bad_list *List
-	if bad_list != nil {
-		t.Error("Unexpectedly created invalid list")
-	}
-}
-
-func TestAppend(t *testing.T) {
-	list := New()
-	if list == nil {
-		t.Error("Failed to create new list")
+    // Test Length().
+	if n := l.Length(); n != -1 {
+		t.Error("unexpectedly passed Length() test with bad pointer")
+		t.Log("\tExpected: -1")
+		t.Log("\tReceived:", n)
 	}
 
-	// Add one item. Test that item was added successfully and that list is displayed correctly.
-	err := list.Append(5)
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "5")
-	checkLength(t, list, 1)
-
-	// Add another item. Test that item was added successfully and that list is displayed correctly.
-	err = list.Append("bananas")
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "5, bananas")
-	checkLength(t, list, 2)
-
-	// Add another item. Test that item was added successfully and that list is displayed correctly.
-	err = list.Append([]float64{3.14, 1.23})
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "5, bananas, [3.14 1.23]")
-	checkLength(t, list, 3)
-}
-
-func TestAppend_mult(t *testing.T) {
-	list := New()
-	if list == nil {
-		t.Error("Failed to create new list")
-	}
-	checkString(t, list, "<empty>")
-	checkLength(t, list, 0)
-
-	// Test adding nothing.
-	err := list.Append()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "<empty>")
-	checkLength(t, list, 0)
-
-	// Test adding one item.
-	err = list.Append(5)
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "5")
-	checkLength(t, list, 1)
-
-	// Test adding multiple items.
-	err = list.Append("bananas", "grapes", 42.9)
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "5, bananas, grapes, 42.9")
-	checkLength(t, list, 4)
-	return
-
-	// Add another item. Test that item was added successfully and that list is displayed correctly.
-	err = list.Append([]float64{3.14, 1.23})
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "5, bananas, [3.14 1.23]")
-	checkLength(t, list, 3)
-}
-
-func TestInsert(t *testing.T) {
-	// First, test correct usage.
-	list := New()
-	if list == nil {
-		t.Error("Failed to create new list")
+    // Test Insert().
+	if err := l.Insert(0, "item"); err == nil {
+		t.Error("unexpectedly passed Insert() test with bad pointer")
 	}
 
-	// Add one item. Test that item was added successfully and that list is displayed correctly and length is correct.
-	err := list.Insert(0, 5)
-	if err != nil {
-		t.Error(err)
+    // Test Append().
+	if err := l.Append("item"); err == nil {
+		t.Error("unexpectedly passed Append() test with bad pointer")
 	}
-	checkString(t, list, "5")
-	checkLength(t, list, 1)
 
-	// Insert another item at the beginning. Test that item was added successfully and that list is displayed correctly
-	// and length is correct.
-	err = list.Insert(0, "bananas")
-	if err != nil {
-		t.Error(err)
+    // Test Remove().
+	if v := l.Remove(0); v != nil {
+		t.Error("unexpectedly passed Remove() test with bad pointer")
+		t.Log("\tExpected: nil")
+		t.Log("\tReceived:", v)
 	}
-	checkString(t, list, "bananas, 5")
-	checkLength(t, list, 2)
 
-	// Add an item in the middle. Test that item was added successfully and that list is displayed correctly and length
-	// is correct.
-	err = list.Insert(1, []float64{3.14, 1.23})
-	if err != nil {
-		t.Error(err)
+    // Test RemoveMatch().
+	if err := l.RemoveMatch("item"); err == nil {
+		t.Error("unexpectedly passed RemoveMatch() test with bad pointer")
 	}
-	checkString(t, list, "bananas, [3.14 1.23], 5")
-	checkLength(t, list, 3)
 
-	// Now, test incorrect usage.
-	err = list.Insert(10, "no way")
-	if err == nil {
-		t.Error("Unexpectedly passed out-of-bounds test")
+    // Test Index().
+	if i := l.Index("item"); i != -1 {
+		t.Error("unexpectedly passed Index() test with bad pointer")
+		t.Log("\tExpected: -1")
+		t.Log("\tReceived:", i)
 	}
-	checkString(t, list, "bananas, [3.14 1.23], 5")
-	checkLength(t, list, 3)
 
-	var bad_list *List
-	err = bad_list.Insert(0, "shouldn't work")
-	if err == nil {
-		t.Error("Unexpectedly passed invalid list test")
+    // Test Exists().
+	if b := l.Exists("item"); b != false {
+		t.Error("unexpectedly passed Exists() test with bad pointer")
+		t.Log("\tExpected: false")
+		t.Log("\tReceived:", b)
 	}
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-}
 
-func TestClear(t *testing.T) {
-	list := New()
-
-	// Add some items to the list.
-	list.Append(5)
-	list.Append("bronto", "stego", 65e6)
-	list.Append("t. rex", 0x0C)
-	checkString(t, list, "5, bronto, stego, 6.5e+07, t. rex, 12")
-	checkLength(t, list, 6)
-	return
-
-	// Test clearing out the items.
-	list.Clear()
-	checkString(t, list, "<empty>")
-	checkLength(t, list, 0)
-
-	// Test clearing a bad list.
-	var bad_list *List
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-
-	bad_list.Clear()
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-
-	// Test clearing a backdoor list.
-	var backdoor List
-	checkString(t, &backdoor, "<empty>")
-	checkLength(t, &backdoor, 0)
-
-	backdoor.Clear()
-	checkString(t, &backdoor, "<empty>")
-	checkLength(t, &backdoor, 0)
-}
-
-func TestRemove(t *testing.T) {
-	list := New()
-	list.Append(1)
-	list.Append(2)
-	list.Append(3)
-	list.Append(4)
-	list.Append(5)
-
-	// Make sure the list was set up correctly.
-	checkString(t, list, "1, 2, 3, 4, 5")
-	checkLength(t, list, 5)
-
-	// Remove the 3rd item.
-	value := list.Remove(2)
-	if value != 3 {
-		t.Error("Error removing 3rd item")
-		t.Log("Expected: 3")
-		t.Log("Received:", value)
+    // Test Copy().
+	if _, err := l.Copy(); err == nil {
+		t.Error("unexpectedly passed Copy() test with bad pointer")
 	}
-	checkString(t, list, "1, 2, 4, 5")
-	checkLength(t, list, 4)
 
-	// Remove the new 3rd item.
-	value = list.Remove(2)
-	if value != 4 {
-		t.Error("Error removing new 3rd item")
-		t.Log("Expected: 4")
-		t.Log("Received:", value)
+    // Test Merge().
+	if err := l.Merge(New()); err == nil {
+		t.Error("unexpectedly passed Merge() test with bad pointer")
 	}
-	checkString(t, list, "1, 2, 5")
-	checkLength(t, list, 3)
 
-	// Try to remove a non-existant item.
-	value = list.Remove(10)
-	if value != nil {
-		t.Error("Unexpectedly passed out-of-bounds test")
-		t.Log("Expected: nil")
-		t.Log("Received:", value)
+    // Test Clear().
+	if err := l.Clear(); err == nil {
+		t.Error("unexpectedly passed Clear() test with bad pointer")
 	}
-	checkString(t, list, "1, 2, 5")
-	checkLength(t, list, 3)
 
-	// Try the remove operation on an invalid list.
-	var bad_list *List
-	value = bad_list.Remove(0)
-	if value != nil {
-		t.Error("Unexpectedly passed invalid list test")
-		t.Log("Expected: nil")
-		t.Log("Received:", value)
+    // Test Sort().
+	if err := l.Sort(func(l, r interface{}) bool {return false}); err == nil {
+		t.Error("unexpectedly passed Sort() test with bad pointer")
+	}
+
+    // Test SortInt().
+	if err := l.SortInt(); err == nil {
+		t.Error("unexpectedly passed SortInt() test with bad pointer")
+	}
+
+    // Test SortStr().
+	if err := l.SortStr(); err == nil {
+		t.Error("unexpectedly passed SortStr() test with bad pointer")
 	}
 }
 
-func TestRemoveMatch(t *testing.T) {
-	list := New()
-	list.Append(1)
-	list.Append("apples")
-	list.Append(3)
-	list.Append(4)
-	list.Append(3.14)
-
-	// Make sure the list was set up correctly.
-	checkString(t, list, "1, apples, 3, 4, 3.14")
-	checkLength(t, list, 5)
-
-	// Remove the "3".
-	if err := list.RemoveMatch(3); err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "1, apples, 4, 3.14")
-	checkLength(t, list, 4)
-
-	// Remove the "1".
-	if err := list.RemoveMatch(1); err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "apples, 4, 3.14")
-	checkLength(t, list, 3)
-
-	// Remove "apples".
-	if err := list.RemoveMatch("apples"); err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "4, 3.14")
-	checkLength(t, list, 2)
-
-	// Remove the "3.14".
-	if err := list.RemoveMatch(3.14); err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "4")
-	checkLength(t, list, 1)
-
-	// Try to remove a non-existant item.
-	if err := list.RemoveMatch(10); err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "4")
-	checkLength(t, list, 1)
-
-	// Try the remove operation on an invalid list.
-	var bad_list *List
-	if err := bad_list.RemoveMatch(1); err == nil {
-		t.Error("Unexpectedly passed invalid list test")
-	}
-}
-
-func TestIndex(t *testing.T) {
-	list := New()
-	list.Append("apples")
-	list.Append(1)
-	list.Append(3)
-	list.Append(3.14)
-	list.Append(4)
-
-	// Make sure the list was set up correctly.
-	checkString(t, list, "apples, 1, 3, 3.14, 4")
-	checkLength(t, list, 5)
-
-	// Check index of 1.
-	index := list.Index(1)
-	if index != 1 {
-		t.Error("Incorrect index for 1")
-		t.Log("Expected: 1")
-		t.Log("Received:", index)
-	}
-
-	// Check index of "apples".
-	index = list.Index("apples")
-	if index != 0 {
-		t.Error("Incorrect index for \"apples\"")
-		t.Log("Expected: 0")
-		t.Log("Received:", index)
-	}
-
-	// Check index of 1.
-	index = list.Index(3.14)
-	if index != 3 {
-		t.Error("Incorrect index for 3.14")
-		t.Log("Expected: 3")
-		t.Log("Received:", index)
-	}
-
-	// Try to find a non-existant item.
-	index = list.Index(10)
-	if index != -1 {
-		t.Error("Unexpectedly passed no-match test")
-	}
-
-	// Try the Index() operation on an invalid list.
-	var bad_list *List
-	index = bad_list.Index(1)
-	if index != -1 {
-		t.Error("Unexpectedly passed invalid list test")
-	}
-}
-
-func TestExists(t *testing.T) {
-	list := New()
-	list.Append(1)
-	list.Append(3)
-	list.Append("apples")
-	list.Append(3.14)
-	list.Append(4)
-
-	// Make sure the list was set up correctly.
-	checkString(t, list, "1, 3, apples, 3.14, 4")
-	checkLength(t, list, 5)
-
-	// Check that 3 exists.
-	ret := list.Exists(3)
-	if !ret {
-		t.Error("Expected match for 3, but didn't find it")
-	}
-
-	// Check that "apples" exists.
-	ret = list.Exists("apples")
-	if !ret {
-		t.Error("Expected match for \"apples\", but didn't find it")
-	}
-
-	// Check that 3.14 exists.
-	ret = list.Exists(3.14)
-	if !ret {
-		t.Error("Expected match for 3,14, but didn't find it")
-	}
-
-	// Try to find a non-existant item.
-	ret = list.Exists(10)
-	if ret {
-		t.Error("Unexpectedly passed no-match test")
-	}
-
-	// Try the Exists() operation on an invalid list.
-	var bad_list *List
-	ret = bad_list.Exists(1)
-	if ret {
-		t.Error("Unexpectedly passed invalid list test")
-	}
-}
-
-func TestCopy(t *testing.T) {
+func TestBadArgs(t *testing.T) {
+	// Test that passing bad values to methods is handled correctly.
 	l := New()
+	checkString(t, l, "<empty>")
+	checkLength(t, l, 0)
 
-	// Copy an empty list.
-	nl, err := l.Copy()
-	if err != nil {
-		t.Error(err)
+    // Test Insert().
+	if err := l.Insert(-1, "item"); err == nil {
+		t.Error("unexpectedly passed Insert() test for negative index")
 	}
-	checkString(t, nl, "<empty>")
-	checkLength(t, nl, 0)
+	checkString(t, l, "<empty>")
+	checkLength(t, l, 0)
 
-	// Copy a non-empty list.
-	l.Append("sizzle", 1e5, 3.1415, 15)
-	checkString(t, l, "sizzle, 100000, 3.1415, 15")
-	checkLength(t, l, 4)
-
-	nl, err = l.Copy()
-	if err != nil {
-		t.Error(err)
+    // Test Remove().
+	if v := l.Remove(-1); v != nil {
+		t.Error("unexpectedly passed Remove() test for negative index")
+		t.Log("\tExpected: nil")
+		t.Log("\tReceived:", v)
 	}
-	checkString(t, nl, "sizzle, 100000, 3.1415, 15")
-	checkLength(t, nl, 4)
+	checkString(t, l, "<empty>")
+	checkLength(t, l, 0)
 
-}
+    // Test Remove().
+	if v := l.Remove(0); v != nil {
+		t.Error("unexpectedly passed Remove() test for empty list")
+		t.Log("\tExpected: nil")
+		t.Log("\tReceived:", v)
+	}
+	checkString(t, l, "<empty>")
+	checkLength(t, l, 0)
 
-func TestMerge(t *testing.T) {
-	// Test merging two good lists togther.
-	list := New()
-	for i := 0; i < 5; i++ {
-		list.Append(i)
+    // Test Remove().
+	if v := l.Remove(100); v != nil {
+		t.Error("unexpectedly passed Remove() test for out-of-range index")
+		t.Log("\tExpected: nil")
+		t.Log("\tReceived:", v)
 	}
-	checkString(t, list, "0, 1, 2, 3, 4")
-	checkLength(t, list, 5)
+	checkString(t, l, "<empty>")
+	checkLength(t, l, 0)
 
-	addition := New()
-	for i := 5; i < 10; i++ {
-		addition.Append(i)
+    // Test Sort().
+	if err := l.Sort(nil); err == nil {
+		t.Error("unexpectedly passed Sort() test for missing sort cb")
 	}
-	checkString(t, addition, "5, 6, 7, 8, 9")
-	checkLength(t, addition, 5)
-
-	err := list.Merge(addition)
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "0, 1, 2, 3, 4, 5, 6, 7, 8, 9")
-	checkString(t, addition, "<empty>")
-	checkLength(t, addition, 0)
-
-	// Test merging a good list and a bad list.
-	list = New()
-	for i := 0; i < 5; i++ {
-		list.Append(i)
-	}
-	checkString(t, list, "0, 1, 2, 3, 4")
-	checkLength(t, list, 5)
-
-	var bad_list *List
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-
-	err = list.Merge(bad_list)
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "0, 1, 2, 3, 4")
-	checkLength(t, list, 5)
-
-	// Test merging a bad list with a good list.
-	// var bad_list *List
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-
-	list = New()
-	for i := 0; i < 5; i++ {
-		list.Append(i)
-	}
-	checkString(t, list, "0, 1, 2, 3, 4")
-	checkLength(t, list, 5)
-
-	err = bad_list.Merge(list)
-	if err == nil {
-		t.Error("Unexpectedly passed bad merge")
-	}
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-	checkString(t, list, "0, 1, 2, 3, 4")
-	checkLength(t, list, 5)
-}
-
-func TestSortInt(t *testing.T) {
-	// Test a power of two.
-	list := New()
-	for i := 1; i <= 8; i++ {
-		list.Insert(0, i)
-	}
-	checkString(t, list, "8, 7, 6, 5, 4, 3, 2, 1")
-	checkLength(t, list, 8)
-	err := list.SortInt()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "1, 2, 3, 4, 5, 6, 7, 8")
-	checkLength(t, list, 8)
-
-	// Test a block with less than one full stack.
-	list = New()
-	for i := 1; i <= 9; i++ {
-		list.Insert(0, i)
-	}
-	checkString(t, list, "9, 8, 7, 6, 5, 4, 3, 2, 1")
-	checkLength(t, list, 9)
-	err = list.SortInt()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "1, 2, 3, 4, 5, 6, 7, 8, 9")
-	checkLength(t, list, 9)
-
-	// Test a block with more than one full stack but less than a full block
-	list = New()
-	for i := 1; i <= 11; i++ {
-		list.Insert(0, i)
-	}
-	checkString(t, list, "11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1")
-	checkLength(t, list, 11)
-	err = list.SortInt()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11")
-	checkLength(t, list, 11)
-
-	// Test small list sizes.
-	list = New()
-	list.Append(1)
-	list.Append(0)
-	checkString(t, list, "1, 0")
-	checkLength(t, list, 2)
-	err = list.SortInt()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "0, 1")
-	checkLength(t, list, 2)
-
-	list = New()
-	list.Append(1)
-	checkString(t, list, "1")
-	checkLength(t, list, 1)
-	err = list.SortInt()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "1")
-	checkLength(t, list, 1)
-
-	// Test invalid lists.
-	list = New()
-	checkString(t, list, "<empty>")
-	checkLength(t, list, 0)
-	err = list.SortInt()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "<empty>")
-	checkLength(t, list, 0)
-
-	var bad_list *List
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-	err = bad_list.SortInt()
-	if err == nil {
-		t.Error("Unexpectedly passed bad list sort")
-	}
-	checkString(t, bad_list, "<nil>")
-	checkLength(t, bad_list, -1)
-}
-
-func TestSortStr(t *testing.T) {
-	// Test uniform length and no same characters.
-	list := New()
-	list.Append("ccc")
-	list.Append("bbb")
-	list.Append("aaa")
-	checkString(t, list, "ccc, bbb, aaa")
-	checkLength(t, list, 3)
-	err := list.SortStr()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "aaa, bbb, ccc")
-	checkLength(t, list, 3)
-
-	// Test variable lengths and no same characters.
-	list = New()
-	list.Append("ccccc")
-	list.Append("bbb")
-	list.Append("aaaaaaaaaaa")
-	checkString(t, list, "ccccc, bbb, aaaaaaaaaaa")
-	checkLength(t, list, 3)
-	err = list.SortStr()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "aaaaaaaaaaa, bbb, ccccc")
-	checkLength(t, list, 3)
-
-	// Test uniform length and similar characters.
-	list = New()
-	list.Append("cabc")
-	list.Append("caab")
-	list.Append("abab")
-	checkString(t, list, "cabc, caab, abab")
-	checkLength(t, list, 3)
-	err = list.SortStr()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "abab, caab, cabc")
-	checkLength(t, list, 3)
-
-	// Test variable length and similar characters.
-	list = New()
-	list.Append("cababcabbcbababca")
-	list.Append("cabacbcabacb")
-	list.Append("cabababacba")
-	checkString(t, list, "cababcabbcbababca, cabacbcabacb, cabababacba")
-	checkLength(t, list, 3)
-	err = list.SortStr()
-	if err != nil {
-		t.Error(err)
-	}
-	checkString(t, list, "cabababacba, cababcabbcbababca, cabacbcabacb")
-	checkLength(t, list, 3)
+	checkString(t, l, "<empty>")
+	checkLength(t, l, 0)
 }
 
 
 // HELPERS
-func checkString(t *testing.T, list *List, want string) {
-	if list.String() != want {
+func checkString(t *testing.T, l *List, want string) {
+	if l.String() != want {
 		t.Error("List contents are incorrect")
 		t.Log("Expected:", want)
-		t.Log("Received:", list)
+		t.Log("Received:", l)
 	}
 }
 
-func checkLength(t *testing.T, list *List, want int) {
-	if list.Length() != want {
+func checkLength(t *testing.T, l *List, want int) {
+	if l.Length() != want {
 		t.Error("Incorrect length")
 		t.Log("Expected:", want)
-		t.Log("Received:", list.Length())
+		t.Log("Received:", l.Length())
 	}
 }
