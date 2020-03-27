9eade0df1a3d641cec3862a6d82fd66b3ebed2f5
Hilde N <snhilde@protonmail.com>
2020-03-27 08:42:23 -0400

    add tests handling a bad pointer to a Table

--- data_structures/htable/htable_test.go
+++ data_structures/htable/htable_test.go
@@ -5,9 +5,40 @@ import (
 )
 
 
-func TestNew(t *testing.T) {
-	n := New()
-	if n == nil {
-		t.Error("Failed to create new table")
+func TestBadPtr(t *testing.T) {
+	var tb *Table
+
+    // Test AddRow().
+	if err := tb.AddRow("a", "b", "c"); err == nil {
+		t.Error("unexpectedly passed bad pointer test for AddRow()")
+	}
+
+    // Test InsertRow().
+	if err := tb.InsertRow(1, "a", "b", "c"); err == nil {
+		t.Error("unexpectedly passed bad pointer test for InsertRow()")
+	}
+
+	// Test RemoveRow().
+	tb.RemoveRow(2)
+
+    // Test Rows().
+	if n := tb.Rows(); n != -1 {
+		t.Error("unexpectedly passed bad pointer test for Rows()")
+	}
+
+    // Test Columns().
+	if n := tb.Columns(); n != -1 {
+		t.Error("unexpectedly passed bad pointer test for Columns()")
+	}
+
+    // Test Count().
+	if n := tb.Count(); n != -1 {
+		t.Error("unexpectedly passed bad pointer test for Count()")
+	}
+
+    // Test Row().
+	if n := tb.Row("a", 5); n != -1 {
+		t.Error("unexpectedly passed bad pointer test for Row()")
 	}
 }
+
