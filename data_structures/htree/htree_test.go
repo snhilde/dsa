ccddc6a206d2cdde6d569b729c163ce35763c333
Hilde N <snhilde@protonmail.com>
2020-04-01 19:42:29 -0400

    add tests for New()

--- data_structures/htree/htree_test.go
+++ data_structures/htree/htree_test.go
@@ -3,3 +3,19 @@ package htree
 import (
 	"testing"
 )
+
+
+func TestNew(t *testing.T) {
+	tr := New()
+	if tr == nil {
+		t.Error("Failed to create tree")
+	}
+
+	if tr.trunk != nil {
+		t.Error("Trunk node is not nil")
+	}
+
+	if tr.length != 0 {
+		t.Error("Tree claims to have nodes")
+	}
+}
