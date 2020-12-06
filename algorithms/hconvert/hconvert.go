59835ca279223b712ac05899ea006500b4bcf516
Hilde N <snhilde@protonmail.com>
2020-12-06 16:09:06 -0500

    add NewConvert, SetDecodeCharSet, and SetEncodeCharSet

--- algorithms/hconvert/hconvert.go
+++ algorithms/hconvert/hconvert.go
@@ -4,6 +4,7 @@ package hconvert
 import (
 )
 
+// Type Converter holds information about the conversion process, including the specified character sets.
 type Converter struct {
 	// binary representation of the number
 	bin []byte
@@ -11,4 +12,29 @@ type Converter struct {
 	// character sets to use for encoding and decoding
 	encCharSet CharSet
 	decCharSet CharSet
+}
+
+// NewConverter creates a new Converter object with the provided character sets. If a character set is not needed (such
+// as when doing only encoding or only decoding), pass `CharSet{}`.
+func NewConverter(encode CharSet, decode CharSet) Converter {
+	c := new(Converter)
+
+	c.encCharSet = encode
+	c.decCharSet = decode
+
+	return *c
+}
+
+// SetDecodeCharSet sets the character set to use for decoding.
+func (c *Converter) SetDecodeCharSet(charSet CharSet) {
+	if c != nil {
+		c.decCharSet = charSet
+	}
+}
+
+// SetEncodeCharSet sets the character set to use for encoding.
+func (c *Converter) SetEncodeCharSet(charSet CharSet) {
+	if c != nil {
+		c.encCharSet = charSet
+	}
 }
