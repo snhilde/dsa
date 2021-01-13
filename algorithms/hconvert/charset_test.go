a121f714b2e7ecc5aca726921ad92cd133da4353
Hilde N <snhilde@protonmail.com>
2021-01-13 08:00:35 -0500

    capitalize ASCII

--- algorithms/hconvert/charset_test.go
+++ algorithms/hconvert/charset_test.go
@@ -180,7 +180,7 @@ func TestString(t *testing.T) {
 
 // Test that the constant character sets have not changed.
 func TestConsts(t *testing.T) {
-	ascii := AsciiCharSet()
+	ascii := ASCIICharSet()
 	asciiTest := []rune{
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 		20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
