29e9595118cf17151320d88f5f4bb40c678eb89a
Hilde N <snhilde@protonmail.com>
2021-01-23 11:00:31 -0500

    add tests for SetDecodeCharSet and SetEncodeCharSet

--- algorithms/hconvert/hconvert_test.go
+++ algorithms/hconvert/hconvert_test.go
@@ -39,10 +39,86 @@ func TestNewConverter(t *testing.T) {
 
 func TestSetDecodeCharSet(t *testing.T) {
 	t.Parallel()
+
+	// Test that you can't set an empty set.
+	dec := hconvert.CharSet{}
+	enc := hconvert.CharSet{}
+	converter := hconvert.NewConverter(dec, enc)
+	if err := converter.SetDecodeCharSet(hconvert.CharSet{}); err == nil {
+		t.Error("Passed setting empty decoding character set")
+	}
+
+	// Test setting a decode char set when one is not already set.
+	dec = hconvert.CharSet{}
+	enc = hconvert.CharSet{}
+	converter = hconvert.NewConverter(dec, enc)
+	if err := converter.SetDecodeCharSet(hconvert.Base16CharSet()); err != nil {
+		t.Error(err)
+	} else if decSet := converter.DecodeCharSet(); !reflect.DeepEqual(decSet, hconvert.Base16CharSet()) {
+		t.Error("Failed to set decoding character set when one is not already set")
+	}
+
+	// Test setting a decode char set when one is already set.
+	dec = hconvert.Base2CharSet()
+	enc = hconvert.CharSet{}
+	converter = hconvert.NewConverter(dec, enc)
+	if err := converter.SetDecodeCharSet(hconvert.Base64URLCharSet()); err != nil {
+		t.Error(err)
+	} else if decSet := converter.DecodeCharSet(); !reflect.DeepEqual(decSet, hconvert.Base64URLCharSet()) {
+		t.Error("Failed to set decoding character set when one is already set")
+	}
+
+	// Test setting a decode char set when both are set.
+	dec = hconvert.Base10CharSet()
+	enc = hconvert.Base4CharSet()
+	converter = hconvert.NewConverter(dec, enc)
+	if err := converter.SetDecodeCharSet(hconvert.Base10CharSet()); err != nil {
+		t.Error(err)
+	} else if decSet := converter.DecodeCharSet(); !reflect.DeepEqual(decSet, hconvert.Base10CharSet()) {
+		t.Error("Failed to set decoding character set when both are set")
+	}
 }
 
 func TestSetEncodeCharSet(t *testing.T) {
 	t.Parallel()
+
+	// Test that you can't set an empty set.
+	dec := hconvert.CharSet{}
+	enc := hconvert.CharSet{}
+	converter := hconvert.NewConverter(dec, enc)
+	if err := converter.SetEncodeCharSet(hconvert.CharSet{}); err == nil {
+		t.Error("Passed setting empty encoding character set")
+	}
+
+	// Test setting an encode char set when one is not already set.
+	dec = hconvert.CharSet{}
+	enc = hconvert.CharSet{}
+	converter = hconvert.NewConverter(dec, enc)
+	if err := converter.SetEncodeCharSet(hconvert.Base58CharSet()); err != nil {
+		t.Error(err)
+	} else if encSet := converter.EncodeCharSet(); !reflect.DeepEqual(encSet, hconvert.Base58CharSet()) {
+		t.Error("Failed to set encoding character set when one is not already set")
+	}
+
+	// Test setting an encode char set when one is already set.
+	dec = hconvert.CharSet{}
+	enc = hconvert.ASCIICharSet()
+	converter = hconvert.NewConverter(dec, enc)
+	if err := converter.SetEncodeCharSet(hconvert.Base10CharSet()); err != nil {
+		t.Error(err)
+	} else if encSet := converter.EncodeCharSet(); !reflect.DeepEqual(encSet, hconvert.Base10CharSet()) {
+		t.Error("Failed to set encoding character set when one is already set")
+	}
+
+	// Test setting an encode char set when both are set.
+	dec = hconvert.Base4CharSet()
+	enc = hconvert.Base10CharSet()
+	converter = hconvert.NewConverter(dec, enc)
+	if err := converter.SetEncodeCharSet(hconvert.Base64CharSet()); err != nil {
+		t.Error(err)
+	} else if encSet := converter.EncodeCharSet(); !reflect.DeepEqual(encSet, hconvert.Base64CharSet()) {
+		t.Error("Failed to set encoding character set when both are set")
+	}
 }
 
 func TestDecodeCharSet(t *testing.T) {
