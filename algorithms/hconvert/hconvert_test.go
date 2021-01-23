03ceb7b2c1ac4b2eb04643576a5d0f0cfa5b9030
Hilde N <snhilde@protonmail.com>
2021-01-23 10:00:43 -0500

    add tests for NewConverter

--- algorithms/hconvert/hconvert_test.go
+++ algorithms/hconvert/hconvert_test.go
@@ -1,13 +1,38 @@
 package hconvert_test
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/snhilde/dsa/algorithms/hconvert"
 )
 
 func TestNewConverter(t *testing.T) {
+	// We should be able to create a Converter with any combination of valid or invalid character
+	// sets.
+	enc := hconvert.CharSet{}
+	dec := hconvert.CharSet{}
+	if converter := hconvert.NewConverter(enc, dec); !reflect.DeepEqual(converter, hconvert.Converter{}) {
+		t.Error("Failed to create empty converter")
+	}
+
+	enc = hconvert.Base16CharSet()
+	dec = hconvert.CharSet{}
+	if converter := hconvert.NewConverter(enc, dec); reflect.DeepEqual(converter, hconvert.Converter{}) {
+		t.Error("Failed to create converter with only encoding character set")
+	}
+
+	enc = hconvert.CharSet{}
+	dec = hconvert.Base32CharSet()
+	if converter := hconvert.NewConverter(enc, dec); reflect.DeepEqual(converter, hconvert.Converter{}) {
+		t.Error("Failed to create converter with only decoding character set")
+	}
+
+	enc = hconvert.Base16CharSet()
+	dec = hconvert.Base32CharSet()
+	if converter := hconvert.NewConverter(enc, dec); reflect.DeepEqual(converter, hconvert.Converter{}) {
+		t.Error("Failed to create converter with both encoding and decoding character sets")
+	}
 }
 
 func TestSetDecodeCharSet(t *testing.T) {
