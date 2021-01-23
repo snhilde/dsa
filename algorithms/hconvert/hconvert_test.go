eb5e0e9558a9d3e155a750a9b84b33ad5557c4a9
Hilde N <snhilde@protonmail.com>
2021-01-23 09:50:11 -0500

    stub out tests

--- algorithms/hconvert/hconvert_test.go
+++ algorithms/hconvert/hconvert_test.go
@@ -6,3 +6,36 @@ import (
 
 	"github.com/snhilde/dsa/algorithms/hconvert"
 )
+
+func TestNewConverter(t *testing.T) {
+}
+
+func TestSetDecodeCharSet(t *testing.T) {
+}
+
+func TestSetEncodeCharSet(t *testing.T) {
+}
+
+func TestDecodeCharSet(t *testing.T) {
+}
+
+func TestEncodeCharSet(t *testing.T) {
+}
+
+func TestDecode(t *testing.T) {
+}
+
+func TestDecodeFrom(t *testing.T) {
+}
+
+func TestDecodeWith(t *testing.T) {
+}
+
+func TestEncode(t *testing.T) {
+}
+
+func TestEncodeTo(t *testing.T) {
+}
+
+func TestEncodeWith(t *testing.T) {
+}
